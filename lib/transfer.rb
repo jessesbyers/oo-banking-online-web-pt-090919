bc58328fcc0ed5161c39001eb42e6a7b3bd634e1
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-01 16:50:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,16 +15,17 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid? && sender.balance < amount
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
-    if valid? && @status == "pending"
+    if valid? && sender.balance > amount && @status == "pending"
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
+
+# sender.valid? && sender.balance < amount
 
   def reverse_transfer
     if @status == "complete"
