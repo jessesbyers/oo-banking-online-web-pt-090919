51187e40a2bb8e925ab9300d7ba4cee2ea8bfb2a
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-01 16:28:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
     elsif valid? && @status == "pending"
         sender.balance = sender.balance - amount
         receiver.balance = receiver.balance + amount
-        @status = "complete" 
+        @status = "complete"
       end
       message
     end
