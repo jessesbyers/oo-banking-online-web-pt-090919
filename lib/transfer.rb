70bec7ebda4c458f729ec8334238621e51e22f65
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-01 16:19:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,10 @@ class Transfer
       end
 
       def reverse_transfer
+        if @status == "complete"
+          sender.balance = sender.balance + amount
+          receiver.balance = receiver.balance - amount
+        end
       end
   end
 
