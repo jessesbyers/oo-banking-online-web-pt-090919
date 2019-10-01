1f6c70acb78707bf9891294713a501bbca646e2a
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-01 16:25:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
         sender.balance = sender.balance - amount
         receiver.balance = receiver.balance + amount
         @status = "complete"
-      elsif !sender.valid?
+      elsif sender.valid? == false
         @status = "rejected"
         message = "Transaction rejected. Please check your account balance."
       end
