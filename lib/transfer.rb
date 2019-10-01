dbe960fa0fa81d3d9e7306354d623293618c61a8
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-01 16:32:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,6 @@ class Transfer
       receiver.balance = receiver.balance + amount
       @status = "complete"
     end
-      "Transaction rejected. Please check your account balance."
   end
 
       def reverse_transfer
